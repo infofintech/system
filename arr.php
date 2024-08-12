977bb64fc435a57ac33167770bfc0a78d1001239
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-08-12 05:37:36 +0000

    Created aliases for same array operations

--- arr.php
+++ arr.php
@@ -2,7 +2,7 @@
 $name = $_REQUEST['name']; $oper = $_REQUEST['oper'];
 $path = $_REQUEST['path']; $val = $_REQUEST['val'];
 $jf = (@json_decode(file_get_contents($name), true) != null) ? json_decode(file_get_contents($name), true) : [];
-if ($oper == 'add') {
+if (($oper == 'create') || ($oper == 'update') || ($oper == 'alter') || ($oper == 'touch') || ($oper == 'make') || ($oper == 'add') || ($oper == '+')) {
     if (strpos($path, '/') !== false) {
         $nodes = explode('/', $path);
         $temp = &$jf;
@@ -20,7 +20,7 @@ if ($oper == 'add') {
             $jf[$path] = $val;
         } $res = $jf;
     }
-} elseif ($oper == 'drop') {
+} elseif (($oper == 'remove') || ($oper == 'delete') || ($oper == 'drop') || ($oper == 'del') || ($oper == 'rm') || ($oper == '-')) {
     if (strpos($path, '/') !== false) {
         $nodes = explode('/', $path);
         $prevEl = NULL; $el = &$jf;
@@ -42,7 +42,7 @@ if ($oper == 'add') {
             unset($jf[$path]);
         } $res = $jf;
     }
-} else if ($oper == 'clear') {
+} else if (($oper == 'clear') || ($oper == 'erase') || ($oper == 'purge') || ($oper == '~')) {
     $res = [ "" => "" ];
 } file_put_contents($name, json_encode($res));
 chmod($name, 0777);
