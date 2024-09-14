24a3c5560e0bf0be1d010da11dccbec1b420833c
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-09-14 19:30:41 +1000

    Latest update

--- change.php
+++ change.php
@@ -7,27 +7,38 @@ if ($id == $to) {
     file_put_contents($id.'_password', $pass);
     chmod($id.'_password', 0777);
 } else {
+    chmod($id.'_password', 0777);
+    unlink($id.'_password');
+    file_put_contents($to.'_password', $pass);
+    chmod($to.'_password', 0777);
     if (!file_exists($to.'_session.json')) {
-        chmod($id.'_password', 0777);
-        unlink($id.'_password');
-        file_put_contents($to.'_password', $pass);
-        chmod($to.'_password', 0777);
         chmod($id.'_session.json', 0777);
         rename($id.'_session.json', $to.'_session.json');
         chmod($to.'_session.json', 0777);
+    }
+    if (!file_exists($to.'_metadata.json')) {
         chmod($id.'_metadata.json', 0777);
         rename($id.'_metadata.json', $to.'_metadata.json');
         chmod($to.'_metadata.json', 0777);
+    }
+    if (!file_exists($to.'_lock.json')) {
+        chmod($id.'_lock.json', 0777);
         rename($id.'_lock.json', $to.'_lock.json');
         chmod($to.'_lock.json', 0777);
-        if (file_exists($id.'_session.json.bak')) {
-            unlink($id.'_session.json.bak');
-        } unlink($id.'_session.json');
-        if (file_exists($id.'_metadata.json.bak')) {
-            unlink($id.'_metadata.json.bak');
-        } unlink($id.'_metadata.json');
-        if (file_exists($id.'_lock.json.bak')) {
-            unlink($id.'_lock.json.bak');
-        } unlink($id.'_lock.json');
-    }
+    } 
+    if (file_exists($id.'_session.json.bak')) {
+        chmod($id.'_session.json.bak', 0777);
+        unlink($id.'_session.json.bak');
+    } chmod($id.'_session.json', 0777);
+    unlink($id.'_session.json');
+    if (file_exists($id.'_metadata.json.bak')) {
+        chmod($id.'_metadata.json.bak', 0777);
+        unlink($id.'_metadata.json.bak');
+    } chmod($id.'_metadata.json', 0777);
+    unlink($id.'_metadata.json');
+    if (file_exists($id.'_lock.json.bak')) {
+        chmod($id.'_lock.json.bak', 0777);
+        unlink($id.'_lock.json.bak');
+    } chmod($id.'_lock.json', 0777);
+    unlink($id.'_lock.json');
 }
