46589a5a18cadf5ae99ea6d48da1aef37f9015cb
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-09-17 01:22:19 +0000

    Updated code

--- change.php
+++ change.php
@@ -1,8 +1,6 @@
 <?php
-$id = $_REQUEST['id'];
-$to = $_REQUEST['to'];
-$pass = $_POST['pass'];
-if ($id == $to) {
+$id = $_REQUEST['id']; $to = $_REQUEST['to'];
+$pass = $_POST['pass']; if ($id == $to) {
     chmod($id.'_password', 0777);
     file_put_contents($id.'_password', $pass);
     chmod($id.'_password', 0777);
@@ -11,34 +9,25 @@ if ($id == $to) {
     unlink($id.'_password');
     file_put_contents($to.'_password', $pass);
     chmod($to.'_password', 0777);
-    if (!file_exists($to.'_session.json')) {
-        chmod($id.'_session.json', 0777);
-        rename($id.'_session.json', $to.'_session.json');
-        chmod($to.'_session.json', 0777);
+    $removal = ['lock','metadata','session'];
+    $sublevels = ['store','book','msgbox'];
+    foreach ($sublevels as $val) {
+        if (file_exists('./.'.$val.'/'.$id.'_'.$val.'.json') && (!file_exists('./.'.$val.'/'.$to.'_'.$val.'.json'))) {
+            chmod('./.'.$val.'/'.$id.'_'.$val.'.json', 0777);
+            rename('./.'.$val.'/'.$id.'_'.$val.'.json', './.'.$val.'/'.$to.'_'.$val.'.json');
+            chmod('./.'.$val.'/'.$to.'_'.$val.'.json', 0777);
+        } if (file_exists('./.'.$val.'/'.$id.'_'.$val.'.json.bak')) {
+            chmod('./.'.$val.'/'.$id.'_'.$val.'.json.bak', 0777);
+            unlink('./.'.$val.'/'.$id.'_'.$val.'.json.bak');
+        }
+    } foreach ($removal as $val) {
+        if (file_exists($id.'_'.$val.'.json') && (!file_exists($to.'_'.$val.'.json'))) {
+            chmod($id.'_'.$val.'.json', 0777);
+            rename($id.'_'.$val.'.json', $to.'_'.$val.'.json');
+            chmod($to.'_'.$val.'.json', 0777);
+        } if (file_exists($id.'_'.$val.'.json.bak')) {
+            chmod($id.'_'.$val.'.json.bak', 0777);
+            unlink($id.'_'.$val.'.json.bak');
+        }
     }
-    if (!file_exists($to.'_metadata.json')) {
-        chmod($id.'_metadata.json', 0777);
-        rename($id.'_metadata.json', $to.'_metadata.json');
-        chmod($to.'_metadata.json', 0777);
-    }
-    if (!file_exists($to.'_lock.json')) {
-        chmod($id.'_lock.json', 0777);
-        rename($id.'_lock.json', $to.'_lock.json');
-        chmod($to.'_lock.json', 0777);
-    } 
-    if (file_exists($id.'_session.json.bak')) {
-        chmod($id.'_session.json.bak', 0777);
-        unlink($id.'_session.json.bak');
-    } chmod($id.'_session.json', 0777);
-    unlink($id.'_session.json');
-    if (file_exists($id.'_metadata.json.bak')) {
-        chmod($id.'_metadata.json.bak', 0777);
-        unlink($id.'_metadata.json.bak');
-    } chmod($id.'_metadata.json', 0777);
-    unlink($id.'_metadata.json');
-    if (file_exists($id.'_lock.json.bak')) {
-        chmod($id.'_lock.json.bak', 0777);
-        unlink($id.'_lock.json.bak');
-    } chmod($id.'_lock.json', 0777);
-    unlink($id.'_lock.json');
 }
