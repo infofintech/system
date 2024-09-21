aaaaca378fbbc34060bc1a4255bfeec9f6aebfd4
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-09-21 15:06:44 +1000

    Updated frontend script

--- frontend.js
+++ frontend.js
@@ -12,21 +12,16 @@ function jsexpr(source) {
 }
 function calc(expr) {
     var res = '', prep, prec, arr = [], rer = [], sol, vars, reg, solt = [];
-    nerdamer.set('SOLUTIONS_AS_OBJECT', true);
-    reg = /\b(?:([a-z])(?!\w))+\b/gi;
+    nerdamer.set('SOLUTIONS_AS_OBJECT', true); reg = /\b(?:([a-z])(?!\w))+\b/gi;
     if (expr.includes(';')) {
-        prep = expr.split(';');
-        for (ib in prep) {
+        prep = expr.split(';'); for (ib in prep) {
             if (prep[ib].includes(',')) {
-                prec = prep[ib].split(',');
-                for (ic in prec) {
+                prec = prep[ib].split(','); for (ic in prec) {
                     if (prep[ib].match(reg) !== null) {
-                        vars = finarr(prep[ib].match(reg));
-                        for (vr in vars) {
+                        vars = finarr(prep[ib].match(reg)); for (vr in vars) {
                             sol = nerdamer.solve(prec[ic], vars[vr]).toString();
                             if (sol.includes(',')) {
-                                solt = sol.split(',');
-                                for (io in solt) {
+                                solt = sol.split(','); for (io in solt) {
                                     arr.push(vars[vr]+'='+jsexpr(solt[io]));
                                 }
                             } else {
@@ -37,38 +32,30 @@ function calc(expr) {
                 }
             } else {
                 if (prep[ib].match(reg) !== null) {
-                    vars = finarr(prep[ib].match(reg));
-                    for (vr in vars) {
+                    vars = finarr(prep[ib].match(reg)); for (vr in vars) {
                         sol = nerdamer.solve(prep[ib], vars[vr]).toString();
                         if (sol.includes(',')) {
                             solt = sol.split(',');
-                            for (io in solt) {
-                                arr.push(vars[vr]+'='+jsexpr(solt[io]));
-                            }
+                            for (io in solt) { arr.push(vars[vr]+'='+jsexpr(solt[io])); }
                         } else {
                             arr.push(vars[vr]+'='+jsexpr(sol));
                         }
                     } rer.push(finarr(arr).filter(item => (item.split('=')[1] != '')).filter(item => !(item.includes('i'))).join(','));
                 } else {
-                    with(Math) {
-                        rer.push(eval(prep[ib]));
-                    }
+                    with(Math) { rer.push(eval(prep[ib])); }
                 }
             }
         } res = finarr(rer).join(';');
     } else {
         if (expr.includes(',')) {
-            prec = expr.split(',');
-            for (ic in prec) {
+            prec = expr.split(','); for (ic in prec) {
                 if (expr.match(reg) !== null) {
                     vars = finarr(expr.match(reg));
                     for (vr in vars) {
                         sol = nerdamer.solve(prec[ic], vars[vr]).toString();
                         if (sol.includes(',')) {
                             solt = sol.split(',');
-                            for (io in solt) {
-                                arr.push(vars[vr]+'='+jsexpr(solt[io]));
-                            }
+                            for (io in solt) { arr.push(vars[vr]+'='+jsexpr(solt[io])); }
                         } else {
                             arr.push(vars[vr]+'='+jsexpr(sol));
                         }
@@ -77,22 +64,17 @@ function calc(expr) {
             }
         } else {
             if (expr.match(reg) !== null) {
-                vars = finarr(expr.match(reg));
-                for (vr in vars) {
+                vars = finarr(expr.match(reg)); for (vr in vars) {
                     sol = nerdamer.solve(expr, vars[vr]).toString();
                     if (sol.includes(',')) {
                         solt = sol.split(',');
-                        for (io in solt) {
-                            arr.push(vars[vr]+'='+jsexpr(solt[io]));
-                        }
+                        for (io in solt) { arr.push(vars[vr]+'='+jsexpr(solt[io])); }
                     } else {
                         arr.push(vars[vr]+'='+jsexpr(sol));
                     }
                 } res = finarr(arr).filter(item => (item.split('=')[1] != '')).filter(item => !(item.includes('i'))).join(',');
             } else {
-                with(Math) {
-                    res = eval(expr);
-                }
+                with(Math) { res = eval(expr); }
             }
         }
     } return res;
@@ -101,34 +83,29 @@ function arrmath(input) {
     var arr = []; var mas = []; var res = [];
     // UNION, Logical OR
     if (input.includes('|')) {
-        arr = input.split('|');
-        for (el in arr) {
+        arr = input.split('|'); for (el in arr) {
             mas = arr[el].split(',');
             res = (el == 0) ? mas : res.concat(mas);
         }
     // INTERSECTION, Logical AND
     } else if (input.includes('&')) {
-        arr = input.split('&');
-        for (el in arr) {
+        arr = input.split('&'); for (el in arr) {
             mas = arr[el].split(',');
             res = (el == 0) ? mas : res.filter(x => mas.includes(x));
         }
     // COMPLEMENT, Logical NOT
     } else if (input.includes('~')) {
-        arr = input.split('~');
-        for (el in arr) {
+        arr = input.split('~'); for (el in arr) {
             mas = arr[el].split(',');
             res = (el == 0) ? mas : res.filter(x => !mas.includes(x));
         }
     // SYMMETRIC DIFFERENCE, Logical XOR
     } else if (input.includes('^')) {
-        arr = input.split('^');
-        for (el in arr) {
+        arr = input.split('^'); for (el in arr) {
             mas = arr[el].split(',');
             res = (el == 0) ? mas : res.filter(x => !mas.includes(x)).concat(mas.filter(x => !res.includes(x)));
         }
-    }
-    return res;
+    } return res;
 }
 function pad(num, size) {
     num = num.toString();
@@ -136,24 +113,19 @@ function pad(num, size) {
     return num;
 }
 function bconv(bin, alpha = '0123456789abcdef') {
-    var alp = alpha.split('');
-    var base = alp.length;
+    var alp = alpha.split(''), base = alp.length;
     var div = bin, quot = 0, rem = 0, res = '';
     while (div > 0) {
         quot = Math.floor(div / base);
         rem = Math.floor(div % base);
-        res = alp[rem] + res;
-        div = quot;
+        res = alp[rem] + res; div = quot;
     } return res;
 }
 function hconv(hex, alpha = '0123456789abcdef') {
-    var alp = alpha.split('');
-    var base = alp.length;
-    var hds = hex.split('').reverse();
-    var res = 0;
-    for (it in hds) {
-        res += alp.indexOf(hds[it]) * base ** it;
-    } return res;
+    var alp = alpha.split(''), base = alp.length;
+    var hds = hex.split('').reverse(), res = 0;
+    for (it in hds) { res += alp.indexOf(hds[it]) * base ** it; }
+    return res;
 }
 function bin2hex(bin, offs = 0, alpha = '0123456789abcdef') {
     var res = ''; if (bin != '') {
@@ -168,8 +140,7 @@ function bin2hex(bin, offs = 0, alpha = '0123456789abcdef') {
 }
 function hex2bin(hex, offs = 0, alpha = '0123456789abcdef') {
     var res = ''; if (hex.includes(' ')) {
-        var bytes = [], pos, off;
-        var str = hex.split(' ');
+        var bytes = [], pos, off, str = hex.split(' ');
         for (i = 0; i < str.length; i++) {
             pos = hconv(str[i], alpha);
             off = Math.abs((pos + 1114112 - Math.abs(offs)) % 1114112);
@@ -184,40 +155,39 @@ function hex2bin(hex, offs = 0, alpha = '0123456789abcdef') {
     } return res;
 }
 function sumstr(str) {
-    var sum = 0;
-    for (i = 0; i < str.length; i++) {
+    var sum = 0; for (i = 0; i < str.length; i++) {
         sum += str.codePointAt(i);
     } return sum;
 }
 function obfstr(str) {
     arr = str.toString().match(/[\s\S]{1,3}/g) || [];
     var lon = arr.length, ltr = 0, lbr = '';
-    for (i = 0; i < lon; i++) {
-        ltr += parseInt(arr[i], 16);
-    } return ltr;
+    for (i = 0; i < lon; i++) { ltr += parseInt(arr[i], 16); }
+    return ltr;
 }
 function gemstr(str) {
     var sum = sumstr(str);
     var word = sum.toString();
     var out = 0, tex = word.split('');
     while (tex.length > 1) {
-        out = 0; for (ch in tex) {
-            out += parseInt(tex[ch]);
-        } tex = out.toString().split('');
+        out = 0; for (ch in tex) { out += parseInt(tex[ch]); }
+        tex = out.toString().split('');
     } return parseInt(tex);
 }
 function arraySearch(needle, haystack) {
     for (key in haystack) {
-        if (haystack.hasOwnProperty(key)) {
-            if ((haystack[key] == needle) && (key != needle)) {
-                return key;
-            }
+        if ((haystack.hasOwnProperty(key)) && (haystack[key] == needle) && (key != needle)) {
+            return key;
         }
     } return false;
 }
+function isAllZero(arr) {
+    for (i = 0; i < arr.length; i++) {
+        if ((arr[i] != 0) && (isInt(arr[i]))) { return false; break; }
+    } return true;
+}
 function rand(min, max) {
-    var minCeiled = Math.ceil(min);
-    var maxFloored = Math.floor(max);
+    var minCeiled = Math.ceil(min); var maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
 function isInt(num) {
@@ -227,86 +197,54 @@ function onlyUnique(value, index, array) {
     return array.indexOf(value) === index;
 }
 function finarr(arr) {
-    return arr.filter(onlyUnique).filter(function(e) {
-        return e
-    });
+    return arr.filter(onlyUnique).filter(function(e) { return e });
 }
 function arrsum(arr) {
-    var res = 0;
-    for (i = 0; i < arr.length; i++) {
-        if (isInt(arr[i])) {
-            res += parseInt(arr[i]);
-        }
-    }
-    return parseInt(res);
+    var res = 0; for (i = 0; i < arr.length; i++) {
+        if (isInt(arr[i])) { res += parseInt(arr[i]); }
+    } return parseInt(res);
 }
 function arrjob(str, y, x) {
-    var arr = str.split(y);
-    var arn = {};
+    var arr = str.split(y), arn = {};
     for (i = 0; i < arr.length; i++) {
-        arf = arr[i].split(x);
-        arn[arf[0]] = arf[1];
-    }
-    return arn;
+        arf = arr[i].split(x); arn[arf[0]] = arf[1];
+    } return arn;
 }
 function arrpack(arr, y, x) {
-    var str = '';
-    for (var prop in arr) {
+    var str = ''; for (var prop in arr) {
         str += prop+x+arr[prop]+y;
-    }
-    return str;
+    } return str;
 }
 function arrvals(str, y, x) {
-    var arr = str.split(y);
-    var arn = {};
+    var arr = str.split(y), arn = {};
     for (i = 0; i < arr.length; i++) {
-        arf = arr[i].split(x);
-        arn[arf[0]] = arf[1];
-    }
-    return Object.values(arn);
+        arf = arr[i].split(x); arn[arf[0]] = arf[1];
+    } return Object.values(arn);
 }
 function arrkeys(str, y, x) {
-    var arr = str.split(y);
-    var arn = {};
+    var arr = str.split(y), arn = {};
     for (i = 0; i < arr.length; i++) {
-        arf = arr[i].split(x);
-        arn[arf[0]] = arf[1];
-    }
-    return Object.keys(arn);
+        arf = arr[i].split(x); arn[arf[0]] = arf[1];
+    } return Object.keys(arn);
 }
 function randomImage(list) {
-    var arr = list.split(';');
-    var last = arr.length - 1;
+    var arr = list.split(';'), last = arr.length - 1;
     var randNum = Math.floor(Math.random() * last);
     return arr[randNum];
 }
 function nextImage(list, elem) {
-    var arr = list.split(';');
-    var last = arr.length - 1;
-    var index = arr.indexOf(elem);
-    var num = 0;
+    var arr = list.split(';'), last = arr.length - 1;
+    var index = arr.indexOf(elem), num = 0;
     num = (index < last) ? (index + 1) : 0;
     return arr[num];
 }
 function miniPager(content, key) {
     var fmla = content.split(/\r?\n/)[key];
-    var res = '';
-    if (fmla !== undefined) {
-        res = fmla;
-    } else {
-        res = '';
-    }
-    return res;
+    return (fmla !== undefined) ? fmla : '';
 }
 function pager(content, key) {
     var fmla = content.split(/\r\n\r\n/)[key];
-    var res = '';
-    if (fmla !== undefined) {
-        res = fmla;
-    } else {
-        res = '';
-    }
-    return res;
+    return (fmla !== undefined) ? fmla : '';
 }
 function countChars(str) {
     return str.replace(/[\u0080-\u10FFFF]/g, "x").length;
