3b2ab000403055673a7c0f2364e575a79e0fde93
Vadim Ponomarev <jesuscaesar@outlook.com>
2024-09-21 22:30:45 +0000

    Upgraded Git package manager scripts

--- get.php
+++ get.php
@@ -5,67 +5,70 @@ $pkg = (isset($_REQUEST['pkg'])) ? $_REQUEST['pkg'] : 'from';
 $repo = (isset($_REQUEST['repo'])) ? $_REQUEST['repo'] : 'system';
 $branch = (isset($_REQUEST['branch'])) ? $_REQUEST['branch'] : '';
 $user = (isset($_REQUEST['user'])) ? $_REQUEST['user'] : 'infofintech';
-if ($action == 'i' || $action == 's' || $action == 'o') {
-    if ($pkg == "from" && $repo != "" && $user != "") {
-        $hostArr = explode('://', $host);
-        $fsockopen = fsockopen($hostArr[1], 80, $errno, $errstr, 10);
-        if ($fsockopen != false) {
-            $fileback = str_replace('./','',(glob('./*.txt')));
-            foreach ($fileback as $key=>$file) {
-                if (file_exists($file)) {
-                    chmod($file, 0777); rename($file, $file.'.bak');
-                    chmod($file.'.bak', 0777);
-                }
-            }
-            if ((file_exists($repo.'.pkg')) && (@json_decode(file_get_contents($repo.'.pkg'), true) != null)) {
-                $package = json_decode(file_get_contents($repo.'.pkg'), true);
-                $files = explode(';', $package['files']);
-                foreach ($files as $key=>$file) {
+if (!file_exists('get.lock')) {
+    if ($action == 'i' || $action == 's' || $action == 'o') {
+        if ($pkg == "from" && $repo != "" && $user != "") {
+            $hostArr = explode('://', $host);
+            file_put_contents('get.lock', ''); chmod('get.lock', 0777);
+            $fsockopen = fsockopen($hostArr[1], 80, $errno, $errstr, 10);
+            if ($fsockopen != false) {
+                $fileback = str_replace('./','',(glob('./*.txt')));
+                foreach ($fileback as $key=>$file) {
                     if (file_exists($file)) {
-                        chmod($file, 0777); unlink($file);
+                        chmod($file, 0777); rename($file, $file.'.bak');
+                        chmod($file.'.bak', 0777);
                     }
-                } chmod($repo.'.pkg', 0777); unlink($repo.'.pkg');
-            } if (file_exists($repo)) {
-                chmod($repo, 0777); rename($repo, $repo.'.d');
-            } if ($branch != '') {
-                exec('git clone -b '.$branch.' '.$host.'/'.$user.'/'.$repo);
-            } else {
-                exec('git clone '.$host.'/'.$user.'/'.$repo);
-            } chmod($repo, 0777); exec('mv '.$repo.'/* $PWD');
-            exec('chmod -R 777 .'); exec('rm -rf '.$repo);
-            if (file_exists($repo.'.d')) {
-                chmod($repo.'.d', 0777); rename($repo.'.d', $repo);
-            } $filepass = str_replace('./','',(glob('./*.md')));
-            foreach ($filepass as $key=>$file) {
-                if (file_exists($file)) { chmod($file, 0777); unlink($file); }
-            } $filerest = str_replace('./','',(glob('./*.txt.bak')));
-            foreach ($filerest as $key=>$file) {
-                if (file_exists($file)) {
-                    chmod($file, 0777);
-                    rename($file, str_replace('.txt.bak', '.txt', $file));
-                    chmod(str_replace('.txt.bak', '.txt', $file), 0777);
                 }
-            }
+                if ((file_exists($repo.'.pkg')) && (@json_decode(file_get_contents($repo.'.pkg'), true) != null)) {
+                    $package = json_decode(file_get_contents($repo.'.pkg'), true);
+                    $files = explode(';', $package['files']);
+                    foreach ($files as $key=>$file) {
+                        if (file_exists($file)) {
+                            chmod($file, 0777); unlink($file);
+                        }
+                    } chmod($repo.'.pkg', 0777); unlink($repo.'.pkg');
+                } if (file_exists($repo)) {
+                    chmod($repo, 0777); rename($repo, $repo.'.d');
+                } if ($branch != '') {
+                    exec('git clone -b '.$branch.' '.$host.'/'.$user.'/'.$repo);
+                } else {
+                    exec('git clone '.$host.'/'.$user.'/'.$repo);
+                } chmod($repo, 0777); exec('mv '.$repo.'/* $PWD');
+                exec('chmod -R 777 .'); exec('rm -rf '.$repo);
+                if (file_exists($repo.'.d')) {
+                    chmod($repo.'.d', 0777); rename($repo.'.d', $repo);
+                } $filepass = str_replace('./','',(glob('./*.md')));
+                foreach ($filepass as $key=>$file) {
+                    if (file_exists($file)) { chmod($file, 0777); unlink($file); }
+                } $filerest = str_replace('./','',(glob('./*.txt.bak')));
+                foreach ($filerest as $key=>$file) {
+                    if (file_exists($file)) {
+                        chmod($file, 0777);
+                        rename($file, str_replace('.txt.bak', '.txt', $file));
+                        chmod(str_replace('.txt.bak', '.txt', $file), 0777);
+                    }
+                }
+            } chmod('get.lock', 0777); unlink('get.lock');
         }
-    }
-} elseif ($action == 'r' || $action == 'p' || $action == 'm') {
-    if ($pkg != "" && $repo != "" && $user != "") {
-        if ((file_exists($pkg.'.pkg')) && (@json_decode(file_get_contents($pkg.'.pkg'), true) != null)) {
-            $package = json_decode(file_get_contents($pkg.'.pkg'), true);
-            $list = $package['files']; $files = explode(";", $list);
-            foreach ($files as $key=>$file) {
-                if (file_exists($file)) { chmod($file, 0777); unlink($file); }
-            } chmod($pkg.'.pkg', 0777); unlink($pkg.'.pkg');
-        } header('Location: get.php?action=i&host='.$host.'&pkg=from&repo='.$repo.'&branch='.$branch.'&user='.$user);
-    }
-} elseif ($action == 'd' || $action == 'u' || $action == 'x') {
-    if ($pkg != "" && $repo == 'from' && $user == 'here') {
-        if ((file_exists($pkg.'.pkg')) && (@json_decode(file_get_contents($pkg.'.pkg'), true) != null)) {
-            $package = json_decode(file_get_contents($pkg.'.pkg'), true);
-            $list = $package['files']; $files = explode(";", $list);
-            foreach ($files as $key=>$file) {
-                if (file_exists($file)) { chmod($file, 0777); unlink($file); }
-            } chmod($pkg.'.pkg', 0777); unlink($pkg.'.pkg');
+    } elseif ($action == 'r' || $action == 'p' || $action == 'm') {
+        if ($pkg != "" && $repo != "" && $user != "") {
+            if ((file_exists($pkg.'.pkg')) && (@json_decode(file_get_contents($pkg.'.pkg'), true) != null)) {
+                $package = json_decode(file_get_contents($pkg.'.pkg'), true);
+                $list = $package['files']; $files = explode(";", $list);
+                foreach ($files as $key=>$file) {
+                    if (file_exists($file)) { chmod($file, 0777); unlink($file); }
+                } chmod($pkg.'.pkg', 0777); unlink($pkg.'.pkg');
+            } header('Location: get.php?action=i&host='.$host.'&pkg=from&repo='.$repo.'&branch='.$branch.'&user='.$user);
+        }
+    } elseif ($action == 'd' || $action == 'u' || $action == 'x') {
+        if ($pkg != "" && $repo == 'from' && $user == 'here') {
+            if ((file_exists($pkg.'.pkg')) && (@json_decode(file_get_contents($pkg.'.pkg'), true) != null)) {
+                $package = json_decode(file_get_contents($pkg.'.pkg'), true);
+                $list = $package['files']; $files = explode(";", $list);
+                foreach ($files as $key=>$file) {
+                    if (file_exists($file)) { chmod($file, 0777); unlink($file); }
+                } chmod($pkg.'.pkg', 0777); unlink($pkg.'.pkg');
+            }
         }
     }
 }
--- getdir.php
+++ getdir.php
@@ -5,27 +5,30 @@ $pkg = (isset($_REQUEST['pkg'])) ? $_REQUEST['pkg'] : 'from';
 $repo = (isset($_REQUEST['repo'])) ? $_REQUEST['repo'] : 'system';
 $branch = (isset($_REQUEST['branch'])) ? $_REQUEST['branch'] : '';
 $user = (isset($_REQUEST['user'])) ? $_REQUEST['user'] : 'infofintech';
-if ($action == 'i' || $action == 's' || $action == 'o') {
-    if ($pkg == "from" && $repo != "" && $user != "") {
-        $hostArr = explode('://', $host);
-        $fsockopen = fsockopen($hostArr[1], 80, $errno, $errstr, 10);
-        if ($fsockopen != false) {
-            if (file_exists($repo)) {
-                exec('chmod -R 777 .'); exec('rm -rf '.$repo);
-            } if ($branch != '') {
-                exec('git clone -b '.$branch.' '.$host.'/'.$user.'/'.$repo);
-            } else {
-                exec('git clone '.$host.'/'.$user.'/'.$repo);
-            } chmod($repo, 0777); exec('chmod -R 777 .');
+if (!file_exists('get.lock')) {
+    if ($action == 'i' || $action == 's' || $action == 'o') {
+        if ($pkg == "from" && $repo != "" && $user != "") {
+            $hostArr = explode('://', $host);
+            file_put_contents('get.lock', ''); chmod('get.lock', 0777);
+            $fsockopen = fsockopen($hostArr[1], 80, $errno, $errstr, 10);
+            if ($fsockopen != false) {
+                if (file_exists($repo)) {
+                    exec('chmod -R 777 .'); exec('rm -rf '.$repo);
+                } if ($branch != '') {
+                    exec('git clone -b '.$branch.' '.$host.'/'.$user.'/'.$repo);
+                } else {
+                    exec('git clone '.$host.'/'.$user.'/'.$repo);
+                } chmod($repo, 0777); exec('chmod -R 777 .');
+            } chmod('get.lock', 0777); unlink('get.lock');
         }
-    }
-} elseif ($action == 'r' || $action == 'p' || $action == 'm') {
-    if ($pkg != "" && $repo != "" && $user != "") {
-        if (file_exists($pkg)) { exec('chmod -R 777 .'); exec('rm -rf '.$pkg); }
-        header('Location: getdir.php?key=i&host='.$host.'&pkg=from&repo='.$repo.'&branch='.$branch.'&user='.$user);
-    }
-} elseif ($action == 'd' || $action == 'u' || $action == 'x') {
-    if ($pkg != "" && $repo == 'from' && $user == 'here') {
-        if (file_exists($pkg)) { exec('chmod -R 777 .'); exec('rm -rf '.$pkg); }
+    } elseif ($action == 'r' || $action == 'p' || $action == 'm') {
+        if ($pkg != "" && $repo != "" && $user != "") {
+            if (file_exists($pkg)) { exec('chmod -R 777 .'); exec('rm -rf '.$pkg); }
+            header('Location: getdir.php?key=i&host='.$host.'&pkg=from&repo='.$repo.'&branch='.$branch.'&user='.$user);
+        }
+    } elseif ($action == 'd' || $action == 'u' || $action == 'x') {
+        if ($pkg != "" && $repo == 'from' && $user == 'here') {
+            if (file_exists($pkg)) { exec('chmod -R 777 .'); exec('rm -rf '.$pkg); }
+        }
     }
 }
