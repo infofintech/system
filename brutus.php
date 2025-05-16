be09e930e327bc9af4588605f7b0dc665488bfbf
Vadim Ponomarev <jesuscaesar@outlook.com>
2025-05-17 07:47:47 +1000

    Adds

--- brutus.php
+++ brutus.php
@@ -2,11 +2,11 @@
 $user=(isset($_REQUEST['user']))?$_REQUEST['user']:'';
 $pass=(isset($_REQUEST['pass']))?$_REQUEST['pass']:'';
 $attr=(isset($_REQUEST['attr']))?$_REQUEST['attr']:'';
-$res=''; if ($attr!='') {
+if ($attr!='') {
     if (preg_match('/admin|root|rw/i',$attr)) {
         if (file_exists($user.'_password')) {
-            $fdata=file_get_contents($user.'_password');
-            if ($pass==$fdata) { $res='OK';
+            $data=file_get_contents($user.'_password');
+            if ($pass==$data) { $res='OK';
             } else { $res='' }
         } else { $res=''; }
     } else { $res=''; }
